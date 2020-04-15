2c425a7feefdd9e6bb9308f9b6f8d7b6024ade2c
JackS19 <basile.cotte@gmail.com>
2020-04-15 12:19:12 +0200

    Binôme de Newton

--- fact.py
+++ fact.py
@@ -5,7 +5,13 @@ def fact(n):
     return res
 
 def coefbin(k,n):
-    if(k>=n):
+    if(k<=n):
         return(fact(n)/(fact(k)*fact(n-k)))
     else:
         return(0)
+
+def new(x,y,n):
+    s=0
+    for i in range(n+1):
+        s+=coefbin(i,n)*(x**k)*(y**(n-k))
+    return(s)
